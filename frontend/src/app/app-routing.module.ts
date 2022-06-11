8a44b4836fa6f924680dee9ba000ed95b3779670
Ashwin Pandey <ashwinp@asiapac.altair.com>
2022-06-11 12:55:45 +0530

    file structure and additional files

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './auth/login/login.component';
+import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { ErrorComponent } from './error/error.component';
+import { TemplatesComponent } from './templates/templates.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: '', component: HomeComponent },
+  { path: 'templates', component: TemplatesComponent },
+  { path: '**', component: ErrorComponent }
 ];
 
 @NgModule({
