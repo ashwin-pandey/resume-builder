070f1b6f3c549d71560d8b8e72672ea391af58bb
jay.bhanushali@quantiphi.com <jaybhanushali3166@gmail.com>
2022-06-12 12:06:04 +0530

    qr generator download added

--- frontend/src/app/resume/qr-modal/qr-modal.component.ts
+++ frontend/src/app/resume/qr-modal/qr-modal.component.ts
@@ -1,5 +1,5 @@
 import { FormServiceService } from './../../services/form-service.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { ToastrService } from 'ngx-toastr';
 
@@ -11,7 +11,13 @@ import { ToastrService } from 'ngx-toastr';
 export class QrModalComponent implements OnInit {
   resumeId: any = '';
   qrData: any = '';
-  constructor(private _auth: AuthService, private _form: FormServiceService, private _toaster: ToastrService) { }
+  naturalWidth: any;
+  naturalHeight: any
+  @ViewChild('canvas', { static: true }) canvas: any;
+  @ViewChild('qrcode', { static: true }) qrcode: ElementRef<HTMLElement> | undefined;
+  image: any;
+  anchor: any;
+  constructor(private _auth: AuthService, private _form: FormServiceService, private _toaster: ToastrService, private renderer: Renderer2,) { }
 
   ngOnInit(): void {
     this.resumeId = this._auth.getResumeId();
@@ -20,7 +26,8 @@ export class QrModalComponent implements OnInit {
         console.log("resss in qr link", res);
 
         if (res) {
-          this.qrData = res.data
+          this.qrData = res.data;
+          this.capture()
         }
       },
       error: (err) => {
@@ -28,5 +35,36 @@ export class QrModalComponent implements OnInit {
       }
 
     })
+  }
+  // Capturing Image
+  capture() {
+    console.log("qr code", this.canvas?.nativeElement.children);
+
+    let canvas: any = this.canvas?.nativeElement;
+    console.log("qr code canvas ", canvas);
+
+    const image = canvas.toDataURL()// Using optional size for image
+
+    console.log("image", image);
+    let a = document.createElement('a');
+    a.href = image;
+    a.download = 'resume.png';
+    this.anchor = a;
+  }
+  download() {
+    if (this.anchor)
+      this.anchor.click()
+  }
+
+
+
+  convertCanvastoFile(dataURL: any) {
+    let blobBin = atob(dataURL.split(',')[1]);
+    let array = [];
+    for (let i = 0; i < blobBin.length; i++) {
+      array.push(blobBin.charCodeAt(i));
+    }
+    let file = new Blob([new Uint8Array(array)], { type: 'image/png' });
+    return file;
   }
 }
