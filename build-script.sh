784fa5460cc148e1b15a233c9e24b15619a72a10
jay.bhanushali@quantiphi.com <jaybhanushali3166@gmail.com>
2022-06-27 22:56:29 +0530

    Deploy to heroku

--- build-script.sh
+++ build-script.sh
@@ -11,7 +11,7 @@ cd ..
 
 # Deploy app to heroku
 rm -rf ../cv-lab-ineuron/!\(.git\)
-cp -rf ./backend/!\(node_modules\) ../cv-lab-ineuron
+cp -rf /backend/!\(node_modules\) ../cv-lab-ineuron
 cd ../cv-lab-ineuron
 git add .
 git commit -m "Deploy to heroku"
