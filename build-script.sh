10d3e69fd9fdb74842d89f275a44c8869c919df5
Abhijeet Pandhe <abhijeet.pandhe@quantiphi.com>
2022-06-27 20:00:04 +0530

    Added deploymeent script

--- build-script.sh
+++ build-script.sh
@@ -7,3 +7,12 @@ rm -rf backend/dist
 cd frontend
 npm run build
 cp -rf ./dist/frontend ../backend/dist 
+cd ..
+
+# Deploy app to heroku
+rm -rf ../cv-lab-ineuron/!\(.git\)
+cp -rf ./backend/!\(node_modules\) ../cv-lab-ineuron
+cd ../cv-lab-ineuron
+git add .
+git commit -m "Deploy to heroku"
+git push heroku master
